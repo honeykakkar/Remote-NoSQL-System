f21d97d3b8ff4782312f6105e1538d9a8a259915
Honey Kakkar <honeykakkar09@gmail.com>
2016-06-01 23:13:42 -0400

    Event package contains code to define Event class

---
+++ Event/Event.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventLogger
+{
+    public class Event
+    {
+        public string EventName;
+        public DateTime EventTime;
+        public string EventOccuredAt;
+        public bool EventPassed;
+        public string EventTriggeredBy;
+    }
+}
